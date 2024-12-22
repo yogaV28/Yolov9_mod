1f2606ef40364a7bca6534b501093c8377ef8822
Yoga Vignesh V <121656366+yogaV28@users.noreply.github.com>
2024-12-22 14:46:51 +0530

    Add files via upload

---
+++ utils/segment/tal/__init__.py
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+version https://git-lfs.github.com/spec/v1
+oid sha256:0486028fa4d1cc658211b606e5935a4c74386a6e9cf5055b812f3484b1b46e57
+size 6
+=======
+# init
+>>>>>>> 36bd69c (Initial commit)
